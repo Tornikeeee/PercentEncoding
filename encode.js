8d00609085e17d935f4c83c2ed5178fabb7e71f6
andr.ia <andriaiaganashvili@gmail.com>
2015-11-27 17:04:45 +0400

    Array input
    array encode

---
+++ encode.js
@@ -0,0 +1,28 @@
+function encode() {
+    URLEncoder = {
+        encode: function (prop) {
+
+            if (Array.isArray(prop)) {
+                var masiv = [];
+                for (var i = 0; i < prop.length; i++) {
+                    var result = percentEncoding(prop[i]);
+                    masiv.push(result);
+                }
+                
+                return 0;
+            }
+        }
+    
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
