7d197b3212c0bfbcf72c660092bb2d9d9aab3a81
andr.ia <andriaiaganashvili@gmail.com>
2015-11-27 18:17:06 +0400

    object encode
    if input is object

--- encode.js
+++ encode.js
@@ -14,7 +14,25 @@ function encode() {
             }
 
             if (typeof prop == 'object') {
-                
+                var masiv1 = [];
+                var masiv2 = [];
+                var masivmerg = "";
+                for (var item in prop) {
+                    var result1 = percentEncoding(item);
+                    var result2 = percentEncoding(property[item]);
+                    masiv1.push(result1);
+                    masiv2.push(result2);
+
+                }
+                for (var i = 0; i < masiv1.length; i++) {
+                    masivmerg += mas1iv[i] + "=" + masiv2[i];
+                    masivmerg += "&";
+                }
+                masivmerg = masivmerg.substring(0, masivmerg.length - 1);
+                console.log(property);
+                console.log(masivmerg);
+                return 0;
+
             }
         }
         
